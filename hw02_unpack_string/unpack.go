613b6a59a775c79d16604af953c284b34fdf497d
Boris Ershov <boris109@gmail.com>
2022-03-11 10:21:36 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,47 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(inputString string) (string, error) {
+	isPreviousChar := true
+	isPreviousEscape := false
+	var charCodeToRepeat rune
+	var responseBuilder strings.Builder
+	for index, inputCharCode := range inputString {
+		switch {
+		case string(inputCharCode) == `\` && !isPreviousEscape:
+			isPreviousEscape = true
+		case !unicode.IsDigit(inputCharCode) && isPreviousEscape && string(inputCharCode) != `\`:
+			return "", ErrInvalidString
+		case !unicode.IsDigit(inputCharCode) || isPreviousEscape:
+			if index != 0 && isPreviousChar {
+				responseBuilder.WriteRune(charCodeToRepeat)
+			}
+			if index == len(inputString)-1 {
+				responseBuilder.WriteRune(inputCharCode)
+			}
+			charCodeToRepeat = inputCharCode
+			isPreviousChar = true
+			isPreviousEscape = false
+		case unicode.IsDigit(inputCharCode) && isPreviousChar:
+			if index == 0 {
+				return "", ErrInvalidString
+			}
+			count, err := strconv.Atoi(string(inputCharCode))
+			if err != nil {
+				return "", ErrInvalidString
+			}
+			responseBuilder.WriteString(strings.Repeat(string(charCodeToRepeat), count))
+			isPreviousChar = false
+		default:
+			return "", ErrInvalidString
+		}
+	}
+	return responseBuilder.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -7,6 +7,12 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestDebug(t *testing.T) {
+	result, err := Unpack(`qwe\45`)
+	require.NoError(t, err)
+	require.Equal(t, `qwe44444`, result)
+}
+
 func TestUnpack(t *testing.T) {
 	tests := []struct {
 		input    string
@@ -17,10 +23,10 @@ func TestUnpack(t *testing.T) {
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
 	}
 
 	for _, tc := range tests {
@@ -34,7 +40,7 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{`3abc`, `45`, `aaa10b`, `qw\ne`}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
